96252069d7266cced1a6147f085e76ae8c34b3c1
tukki <tukki0210@gmail.com>
2020-12-25 07:08:42 +0000

    date修正

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -3,12 +3,22 @@ import path from 'path'
 import matter from 'gray-matter'
 import remark from 'remark'
 import html from 'remark-html'
+import Post from '../pages/posts/[id]'
 
 const postsDirectory = path.join(process.cwd(), 'posts')
+
+
+interface PostData{
+    id: string,
+    title: string,
+    date: Date,
+    image: string
+};
 
 export function getSortedPostsData() {
     //Get file names under /posts
     const fileNames = fs.readdirSync(postsDirectory)
+
     const allPostsData = fileNames.map(fileName => {
         //remove ".md" from file name to get id
         const id = fileName.replace(/\.md$/, '')
@@ -19,24 +29,26 @@ export function getSortedPostsData() {
 
         //Use gray-matter to parse the post metadata section
         const matterResult = matter(fileContents)
-
+        console.log(matterResult.data)
         // Combine the data with the id
+         
         return {
-            id,
-            ...matterResult.data
+            id, ...matterResult.data
         }
     })
 
+    console.log(allPostsData)
+
     // Sort posts by date
 
-    return allPostsData.sort((a,b) => {
+    return allPostsData.sort((a:date,b:date) => {
+
         if (a.date < b.date) {
-            return 1
+            return 1 
         } else {
             return -1
         }
     })
-    // return allPostsData
 }
 
 export function getAllPostIds() {

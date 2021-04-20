966a811482467d5f3cbb4661f87b0ee7bb33ce6e
tukki <tukki0210@gmail.com>
2021-04-20 03:08:47 +0000

    layout調整

--- components/layout.tsx
+++ components/layout.tsx
@@ -12,10 +12,9 @@ type Props = {
     children?: ReactNode
 }
 
-
 const Layout = ({ children }: Props) => {
     return (
-        <div className={"container p-0 m-auto"}>
+        <html lang="ja" >
             <Head>
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
@@ -37,14 +36,14 @@ const Layout = ({ children }: Props) => {
             </Head>
             <header className={"flex flex-col items-center bg-gray-50"}>
                 <Link href="/">
-                    <h1 className={'text-black text-4xl font-sans .font-extrabold'}>{siteTitle}</h1>
+                    <h1 className={'　text-black text-4xl font-sans .font-extrabold'}>{siteTitle}</h1>
                 </Link>
             </header>
             {/* lg(1024px)以上の画面サイズでflexboxに変更 */}
             <div className={'block lg:flex justify-between mb-1'}>
-                <main className={'lg:w-10/12'}>{children}</main>
+                <main className={'lg:w-15/18'}>{children}</main>
 
-                <aside className={'lg:w-2/12 px-2 my-14'}>
+                <aside className={'lg:w-2/18 px-2 my-14'}>
                     {/* プロフィールカード */}
                     <div className={"bg-gray-50 overflow-hidden shadow max-w-xs my-3bg-gray-50"}>
                         <Image className="w-auto"
@@ -72,21 +71,30 @@ const Layout = ({ children }: Props) => {
 
                     {/* カテゴリ */}
                     <div className={"bg-gray-50 mt-2 p-4 "}>
-                        <h3>カテゴリー</h3>
-
-                        {/* <ul className={""}>カメラの話
-                                <li>Nikon</li>
-                            <li>レンズ</li>
+                        <h3 className="border-solid border-0 border-b-2 border-indigo-400 ">カテゴリー</h3>
+                        <h4>プログラミングの話</h4>
+                        <ul className={"list-none mb-10"}>
+                            <li className=""><a className={"block p-2"} href="#">Java</a></li>
+                            <li><a className={"block p-2"} href="#">JavaScript</a></li>
+                            <li><a className={"block p-2"} href="#">Node.js</a></li>
+                            <li><a className={"block p-2"} href="#">React</a></li>
+                            <li><a className={"block p-2"} href="#">Next.js</a></li>
                         </ul>
-                        <ul>プログラミングの話
-                                <li>Java</li>
-                            <li>React</li>
-                        </ul> */}
+                        <h4>読書記録</h4>
+                        <ul className={"list-none mb-10"}>
+                            <li><a className={"block p-2"} href="#">ビジネス書</a></li>
+                            <li><a className={"block p-2"} href="#">小説</a></li>
+                        </ul>
+                        <h4>カメラの話</h4>
+                        <ul className={"list-none mb-10"}>
+                            <li><a className={"block p-2"} href="#">カメラ全般</a></li>
+                            <li><a className={"block p-2"} href="#">レンズ</a></li>
+                        </ul>
                     </div>
                     {/* カテゴリ終わり */}
                 </aside>
             </div>
-        </div>
+        </html>
     )
 }
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
 import '../styles/global.css'
 import '../styles/normalize.css'
 import React from 'react'
-import {AppProps} from 'next/app'
+import { AppProps } from 'next/app'
 
-const App = ({Component, pageProps}:AppProps)=>{
+const App = ({ Component, pageProps }: AppProps) => {
     return <Component {...pageProps} />
 }
 

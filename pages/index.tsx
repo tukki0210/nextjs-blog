a351b06f48a4e2fc70e51c9b2ac3c0e9de60b7ec
tukki <tukki0210@gmail.com>
2021-04-23 02:37:32 +0000

    title

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,44 +8,44 @@ import { getSortedPostsData } from '../lib/posts'
 
 const Home = ({ allPostsData }) => {
   return (
-    <div>
-      <title>{siteTitle}</title>
-      <Layout>
-        <div className={""}>
-          <div className={"flex flex-wrap"}>
+    <Layout>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <div className={""}>
+        <div className={"flex flex-wrap"}>
 
-            {/* 記事カード */}
-            {allPostsData.map(({ id, title, date, image }) => (
-              // 各記事の間隔
-              <div className={" w-auto md:w-1/2 lg:w-1/3"} key={id}>
-                {/* 各記事カードのスタイル */}
-                <div className={"mb-4 mr-4 bg-gray-100 overflow-hidden shadow-lg hover:opacity-80 "} >
-                  <Link href={`/posts/${id}`}>
-                    <div>
-                      <Image className={"block  bg-cover bg-center transition-transform duration-500 ease-in-out"}
-                        src={image}
-                        alt={""}
-                        width={'450'}
-                        height={'300'}
-                      />
-                      {/* タイトルカード */}
-                      <div className={"flex items-center h-12 justify-between leading-tight p-2"}>
-                        <p>{title}</p>
-                      </div>
-                      <small className={"text-grey-darker text-sm pl-4"}>
-                        <Date dateString={date} />
-                      </small>
-                      {/* タイトルカード終わり */}
+          {/* 記事カード */}
+          {allPostsData.map(({ id, title, date, image }) => (
+            // 各記事の間隔
+            <div className={" w-auto md:w-1/2 lg:w-1/3"} key={id}>
+              {/* 各記事カードのスタイル */}
+              <div className={"mb-4 mr-4 bg-gray-100 overflow-hidden shadow-lg hover:opacity-80 "} >
+                <Link href={`/posts/${id}`}>
+                  <div>
+                    <Image className={"block  bg-cover bg-center transition-transform duration-500 ease-in-out"}
+                      src={image}
+                      alt={""}
+                      width={'450'}
+                      height={'300'}
+                    />
+                    {/* タイトルカード */}
+                    <div className={"flex items-center h-12 justify-between leading-tight p-2"}>
+                      <p>{title}</p>
                     </div>
-                  </Link>
-                </div>
+                    <small className={"text-grey-darker text-sm pl-4"}>
+                      <Date dateString={date} />
+                    </small>
+                    {/* タイトルカード終わり */}
+                  </div>
+                </Link>
               </div>
-            ))}
-            {/* 記事カード終わり */}
-          </div>
+            </div>
+          ))}
+          {/* 記事カード終わり */}
         </div>
-      </Layout >
-    </div>
+      </div>
+    </Layout >
   )
 }
 

14c75d8e1bc493457aec09e5ddd1b2def0117f9b
tukki <tukki0210@gmail.com>
2021-06-11 03:30:00 +0000

    dateをanyに

--- components/date.tsx
+++ components/date.tsx
@@ -1,7 +1,7 @@
 import { parseISO, format, Locale } from 'date-fns'
 import ja from 'date-fns/locale/ja'
 
-const Date = ({ dateString }) => {
+const Date = ({ dateString }: any) => {
     const date = parseISO(dateString)
     return <time dateTime={dateString}>{format(date, 'yyyy年 MM月 dd日', { locale: ja })}</time>
 }

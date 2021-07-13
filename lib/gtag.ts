92beb96f6eb3e40a38954806a1b9c06fe729e2e0
tukki <tukki0210@gmail.com>
2021-07-13 04:01:44 +0000

    ga

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -3,7 +3,6 @@ import { Event } from "../types/googleAnalytics/event";
 
 export const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID ?? ''
 
-console.log(GA_ID)
 // IDが取得できない場合
 export const existsGaId = GA_ID !== "";
 

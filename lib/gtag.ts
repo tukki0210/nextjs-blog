c7bc11bd3f480a74748260eec6e10b395c30d36c
tukki <tukki0210@gmail.com>
2021-07-14 00:51:54 +0000

    eslintdisable

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import { Event } from "../types/googleAnalytics/event";
 

83aa4f1f774f20fdbbad4f2453eb943ed29b87a9
tukki <tukki0210@gmail.com>
2022-11-15 08:09:55 +0000

    ライブラリのアップデートとreact-markdownの破壊的変更の修正

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,18 +1,21 @@
 /// <reference types="next" />
-/// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
 
 //windowオブジェクトにgtagを追加している
 interface Window  {
-  // pageviewのため
-  gtag(type: 'config', googleAnalyticsId: string, { page_path: string })
-  // eventのため
-  gtag(
-    type: 'event',
-    eventAction: string,
-    fieldObject: {
-      event_label: string
-      event_category: string
-      value?: string
-    }
-  )
-}
+    // pageviewのため
+    gtag(type: 'config', googleAnalyticsId: string, { page_path: string })
+    // eventのため
+    gtag(
+      type: 'event',
+      eventAction: string,
+      fieldObject: {
+        event_label: string
+        event_category: string
+        value?: string
+      }
+    )
+  }
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,19 +1,17 @@
-import React, { FC } from 'react'
-import Head from 'next/head'
-
+import React, { FC } from 'react';
+import Head from 'next/head';
 import ReactMarkdown from 'react-markdown';
-import Image from "next/image";
-import { GetStaticPaths, InferGetStaticPropsType, GetStaticPropsContext } from 'next'
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import markdownStyles from "../../styles/markdown-styles.module.css"
-import DateComponent from '../../components/date'
-import { getAllPostIds, getPostDataById } from '../../lib/posts'
-import Layout from '../../components/layout'
+import { GetStaticPaths, InferGetStaticPropsType, GetStaticPropsContext } from 'next';
+import markdownStyles from '../../styles/markdown-styles.module.css';
+import DateComponent from '../../components/Molecules/DateBox';
+import { getAllPostIds, getPostDataById } from '../../lib/posts';
+import Layout from '../../components/pages/layout';
+import CodeBlock from '../../components/Molecules/CodeBlock';
+import rehypeRaw from 'rehype-raw';
 
-
-const ImageInMarkDown = ({ src, alt }: { src: string; alt: string }) => <Image src={src} alt={alt} width="600" height="450" />
-
-const CodeBlock = ({ language, value }: { language: string, value: string }) => <SyntaxHighlighter language={language}>{value}</SyntaxHighlighter>
+// const ImageInMarkDown = ({ src, alt }: { src: string; alt: string }) => (
+//   <Image src={src} alt={alt} width='600' height='450' />
+// );
 
 // getStaticPropsはサーバサイドで実行される
 // 静的なファイルを事前にビルドする
@@ -21,31 +19,33 @@ const CodeBlock = ({ language, value }: { language: string, value: string }) => 
 // ルーティング情報が入ったparamsを受け取る
 // InferGetStaticPropsType<typeof getStaticProps>で、
 // getStaticProps()の返り値をもとにPageに渡される型を類推してくれる。
-type Props = InferGetStaticPropsType<typeof getStaticProps>
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
-type Date = `${number}-${number}-${number}`
+type Date = `${number}-${number}-${number}`;
 
 type PostData = {
-  id: string,
-  title: string,
-  date: Date,
-  image: string,
-  metaDescription: string,
-  tags: Array<string>,
-  content: string
+  id: string;
+  title: string;
+  date: Date;
+  image: string;
+  metaDescription: string;
+  tags: Array<string>;
+  content: string;
 };
 
 // GetStaticPropsContext でpostDataの型推論
-export const getStaticProps = async ({ params }: GetStaticPropsContext): Promise<{ props: { postData: PostData; }; }> => {
+export const getStaticProps = async ({
+  params,
+}: GetStaticPropsContext): Promise<{ props: { postData: PostData } }> => {
   // Fetch necessary data for the blog post using params.id
   // params.id はファイル名の[id].tsx に対応する
-  const postData = await getPostDataById(params?.id as string)
+  const postData = await getPostDataById(params?.id as string);
 
   return {
     props: {
       // ページコンポーネントにpropsとして渡される
-      postData
-    }
+      postData,
+    },
   };
 };
 
@@ -53,39 +53,49 @@ export const getStaticProps = async ({ params }: GetStaticPropsContext): Promise
 // getStaticPathsはgetStaticPropsと一緒に使う必要がある
 export const getStaticPaths: GetStaticPaths = async () => {
   // Return a list of possible value for id
-  const paths = await getAllPostIds()
+  const paths = await getAllPostIds();
 
   return {
     paths,
-    fallback: false
-  }
-}
+    fallback: false,
+  };
+};
 
 const Post: FC<Props> = ({ postData }) => (
   <Layout pagetitle={postData.title} metaDescription={postData.metaDescription}>
     <Head>
       <title>{postData.title}</title>
     </Head>
-    <article className="bg-white lg:w-3/4 mx-auto m-10 p-4 md:p-10">
-      <div className="mb-10 px-4 text-xl leading-normal border-solid border-0 border-l-8 border-blue-600">
-        <h2 className="mb-14">{postData.title}</h2>
-        <div className="text-gray-900 flex flex-row-reverse px-2  ">
+    <article className='bg-white lg:w-3/4 mx-auto m-10 p-4 md:p-10'>
+      <div className='mb-10 px-4 text-xl leading-normal border-solid border-0 border-l-8 border-blue-600'>
+        <h2 className='mb-14'>{postData.title}</h2>
+        <div className='flex flex-row-reverse px-2  '>
           <DateComponent dateString={postData.date} />
         </div>
-        <div className="flex -mt-10 text-lg">タグ：{postData.tags.map(tag => 
-          <div key={tag} className="px-2 mx-2 bg-white border-solid border-1 border-blue-600 rounded-2xl">{tag}</div>)}
+        <div className='flex -mt-10 text-lg'>
+          タグ：
+          {postData.tags.map((tag) => (
+            <div
+              key={tag}
+              className='px-2 mx-2 bg-white border-solid border-1 border-blue-600 rounded-2xl'
+            >
+              {tag}
+            </div>
+          ))}
         </div>
       </div>
       <ReactMarkdown
-        // eslint-disable-next-line
+        rehypePlugins={[rehypeRaw]}
         className={markdownStyles.markdown}
-        source={postData.content}
-        allowDangerousHtml
-        renderers={{ code: CodeBlock, image: ImageInMarkDown }}
+        children={postData.content}
+        components={{
+          code: CodeBlock,
+          // image: ImageInMarkDown
+        }}
       />
     </article>
   </Layout>
-)
+);
 
 export default Post;
 

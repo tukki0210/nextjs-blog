a9a60cb9d5213219b53ce10fffbdeec110660577
tukki <tukki0210@gmail.com>
2023-02-09 02:31:40 +0000

    bundleanalyzer

--- next.config.js
+++ next.config.js
@@ -9,3 +9,8 @@ module.exports = {
         NEXT_PUBLIC_GOOGLE_ANALYTICS_ID: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID,
     }
 }
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+  })
+  module.exports = withBundleAnalyzer({})

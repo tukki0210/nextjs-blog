6da1fb48cc995c41093a249ae5ce49b0e5907c07
tukki0210 <tukki0210@gmal.com>
2024-11-06 10:23:27 +0900

    date-fns

--- components/Molecules/DateBox.tsx
+++ components/Molecules/DateBox.tsx
@@ -1,11 +1,11 @@
 import { parseISO, format } from 'date-fns'
-import ja from 'date-fns/locale/ja'
+import { ja } from 'date-fns/locale'
 
 type Date = `${number}-${number}-${number}`
 
 const DateBox = ({ dateString }: { dateString: Date }) => {
     const date = parseISO(dateString)
 
-    return <time dateTime={dateString} className="text-gray-600 ">{format(date, 'yyyy年 MM月 dd日', { locale:ja })}</time>
+    return <time dateTime={dateString} className="text-gray-600 ">{format(date, 'yyyy年 MM月 dd日', { locale: ja })}</time>
 }
 export default DateBox;
